5a532b5a820b545fcadb6eb5f1865366f2039202
HaRam Yoon <haram.yoon@g2-inc.om>
2020-01-27 10:45:58 -0500

    convert primitive function component to class component

--- app/src/generate/lib/structure/editors/primitive.js
+++ app/src/generate/lib/structure/editors/primitive.js
@@ -20,101 +20,128 @@ import {
 import OptionsModal from './OptionsModal'
 
 // Primitive Editor
-const PrimitiveEditor = props => {
+class PrimitiveEditor extends Component {
 
-  const [isOpen, toggleModal] = useState(false);
-  const [values, setState] = useState({
-    name: (props.value && typeof(props.value) === 'object') ? props.value[0]: '',
-    type: (props.value && typeof(props.value) === 'object') ? props.value[1]: '',
-    options: (props.value && typeof(props.value) === 'object') ? props.value[2]: [],
-    comment: (props.value && typeof(props.value) === 'object') ? props.value[3]: ''
-  });
+  constructor(props) {
+    super(props)
 
-  // if ((props.value && typeof(props.value) === 'object')) {
-  //   console.log('in hereerrer');
-  //   setState({
-  //     name: props.value[0],
-  //     type: props.value[1],
-  //     options: props.value[2],
-  //     comment: props.value[3]
-  //   })
-  // }
-
-  // useEffect(() => {
-  //   if ((props.value && typeof(props.value) === 'object')) {
-  //     console.log('in hereerrer');
-  //     setState({
-  //       name: props.value[0],
-  //       type: props.value[1],
-  //       options: props.value[2],
-  //       comment: props.value[3]
-  //     })
-  //   }
-  // })
-
-
-  const removeAll = e => props.remove(props.dataIndex)
-
-  const onChange = e => {
-    let key = e.target.placeholder.toLowerCase()
-    let value = e.target.value
-    if (key === 'options') {
-      value = value.split(/,\s+?/)
+    this.state = {
+      values: {
+        name: '',
+        type: '',
+        options: [],
+        comment: '',
+      },
+      modal : false
     }
 
-    setState(prevState => ({
-      ...prevState,
-      [key] : value
-    }))
-
-    props.change(values, props.dataIndex)
+    this.removeAll = this.removeAll.bind(this)
+    this.onChange = this.onChange.bind(this)
+    this.toggleModal = this.toggleModal.bind(this)
+    this.saveModal = this.saveModal.bind(this)
   }
 
-  const saveModal = (data) => {
-    toggleModal();
+  componentDidMount() {
+    this.initState()
+  }
 
-    console.log(data, 'data in here');
-    setState(prevState => ({
+  initState() {
+    if (this.props.value && typeof(this.props.value) === 'object') {
+      let updateValues = {}
+      if (this.props.value[0] !== this.state.values.name) updateValues.name = this.props.value[0]
+      if (this.props.value[1] !== this.state.values.type) updateValues.type = this.props.value[1]
+      if (this.props.value[2] !== this.state.values.options) updateValues.options = this.props.value[2]
+      if (this.props.value[3] !== this.state.values.comment) updateValues.comment = this.props.value[3]
+
+      if (Object.keys(updateValues).length > 0) {
+        this.setState(prevState => ({
+          values: {
+            ...prevState.values,
+            ...updateValues
+          }
+        }))
+      }
+    }
+  }
+
+  onChange(e) {
+    let key = e.target.placeholder.toLowerCase()
+    let value = e.target.value
+    
+    this.setState(prevState => ({
       values: {
         ...prevState.values,
-        options: [data]
+        [key]: value
       }
-    }))
+    }), () => {
+      if (this.props.change) {
+        this.props.change(this.state.values, this.props.dataIndex)
+      }
+    })
   }
 
-  return (
-    <div className='border m-1 p-1'>
-      <ButtonGroup size='sm' className='float-right'>
-        <Button color='danger' onClick={ removeAll } >
-          <FontAwesomeIcon icon={ faMinusCircle } />
-        </Button>
-      </ButtonGroup>
+  removeAll(e) {
+    this.props.remove(this.props.dataIndex)
+  }
 
-      <div className='border-bottom mb-2'>
-        <h3 className='col-sm-10 my-1'>{ `${values.name}(${values.type})` }</h3>
-      </div>
+  toggleModal() {
+    this.setState({
+      modal : !this.state.modal
+    });
+  }
 
-      <div className='row m-0'>
-        <FormGroup className='col-md-4'>
-          <Label>Name</Label>
-          <Input type="string" placeholder="Name" value={ values.name } onChange={ onChange } />
-        </FormGroup>
+  saveModal(data) {
+    this.toggleModal();
 
-        <FormGroup className='col-md-4'>
-            <Label>&nbsp;</Label>
-            <InputGroup>
-              <Button outline color='info' onClick={ () => toggleModal(!isOpen) }>Type Options</Button>
-              <OptionsModal isOpen={ isOpen } toggleModal={ () => toggleModal(!isOpen) } saveModal={ saveModal } />
-            </InputGroup>
+    data = data.split(/,\s+?/);
+
+    this.setState(prevState => ({
+      values: {
+        ...prevState.values,
+        options: data
+      }
+    }), () => {
+      if (this.props.change) {
+        this.props.change(this.state.values, this.props.dataIndex)
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className='border m-1 p-1'>
+        <ButtonGroup size='sm' className='float-right'>
+          <Button color='danger' onClick={ this.removeAll } >
+            <FontAwesomeIcon icon={ faMinusCircle } />
+          </Button>
+        </ButtonGroup>
+
+        <div className='border-bottom mb-2'>
+          <h3 className='col-sm-10 my-1'>{ `${this.state.values.name}(${this.state.values.type})` }</h3>
+        </div>
+
+        <div className='row m-0'>
+          <FormGroup className='col-md-4'>
+            <Label>Name</Label>
+            <Input type="string" placeholder="Name" value={ this.state.values.name } onChange={ this.onChange } />
           </FormGroup>
 
-        <FormGroup className='col-md-4'>
-          <Label>Comment</Label>
-          <Input type="textarea" placeholder="Comment" rows={ 1 } value={ values.comment } onChange={ onChange } />
-        </FormGroup>
+          <FormGroup className='col-md-4'>
+              <Label>&nbsp;</Label>
+              <InputGroup>
+                <Button outline color='info' onClick={ this.toggleModal }>Type Options</Button>
+                <OptionsModal isOpen={ this.state.modal } toggleModal={ this.toggleModal } saveModal={ this.saveModal } />
+              </InputGroup>
+            </FormGroup>
+
+          <FormGroup className='col-md-4'>
+            <Label>Comment</Label>
+            <Input type="textarea" placeholder="Comment" rows={ 1 } value={ this.state.values.comment } onChange={ this.onChange } />
+          </FormGroup>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 PrimitiveEditor.defaultProps = {
