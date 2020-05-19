1920b3f7eca58ad3a9577db875c63d73e2c5b750
Haram Yoon <haram.yoon.94@gmail.com>
2020-05-19 14:06:58 -0400

    Remove OpenC2 log

--- app/src/static/nav.js
+++ app/src/static/nav.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import logo from '../../resources/images/openc2-logo.png';
+import logo from '../../../resources/icon.png';
 
 const Nav = (props, context) => (
   <nav className="navbar navbar-expand-md navbar-dark bg-primary fixed-top" >
     <a className="navbar-brand" href="#">
-      <img src={ logo } alt='OpenC2 Logo' />
+      <img src={ logo } alt='OpenC2 Logo' style={{ paddingRight: 10 }}/>
+      Schema Generator
     </a>
     <button
       className="navbar-toggler"
@@ -24,3 +25,6 @@ Nav.contextTypes = {
 };
 
 export default connect()(Nav);
+
+
+
